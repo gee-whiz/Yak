2543513f7ace5bb055b13185a4a61c2a89f78e87
George <george@immedia.co.za>
2017-07-28 02:44:00 +0200

    initial commit

---
+++ Yak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yak
+//
+//  Created by George Kapoya on 2017/07/28.
+//  Copyright © 2017 George Kapoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
